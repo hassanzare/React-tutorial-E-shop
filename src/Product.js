67bf013e30b5a2628fcf9334985be09e5cc2f4eb
hassan <zare@yahoo.com>
2022-10-27 07:47:25 +0330

    add_Home__Product

---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import "./Product.css";
+
+function Product({ id, title, price, rating, image }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} />
+      <button>Add Basket</button>
+    </div>
+  );
+}
+
+export default Product;
